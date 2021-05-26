4a51522e499e1b3c90641fde3de9aeef48e8f540
Scarlet Norberg <scarletnorberg2014@gmail.com>
2021-05-26 16:42:18 -0500

    Adding in the python config files and the source script

---
+++ Script_RunGeantSettings.sh
@@ -0,0 +1,5 @@
+cmsRun PPD-RunIISummer20UL17SIM-0_015_cfg.py &> PPD-RunIISummer20UL17SIM-0_015_cfg.txt 
+cmsRun PPD-RunIISummer20UL17SIM-0_2_cfg.py &> PPD-RunIISummer20UL17SIM-0_2_cfg.txt
+cmsRun PPD-RunIISummer20UL17SIM-0_25_cfg.py &> PPD-RunIISummer20UL17SIM-0_25_cfg.txt
+cmsRun PPD-RunIISummer20UL17SIM-0_3_cfg.py &> PPD-RunIISummer20UL17SIM-0_3_cfg.txt
+cmsRun PPD-RunIISummer20UL17SIM-0_45_cfg.py &> PPD-RunIISummer20UL17SIM-0_45_cfg.txt
